2d0b931718229f529f95a34132a5be6234576fd5
Richard Agarici <laurentiu.agarici@gmail.com>
2023-06-18 09:55:17 +0200

    first commit

---
+++ src/StreamsDemo2.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class StreamsDemo {
+    public static void show() {
+        List<Movie> movies = List.of(new Movie("a", 10), new Movie("b", 15), new Movie("c", 20));
+
+        //imperative programming.
+        int count = 0;
+        for (var movie : movies)
+            if (movie.getLikes() > 10)
+                count++;
+
+
+        //declarative (Functional) Programming
+        var count2 = movies.stream()
+                .filter(movie -> movie.getLikes() > 10)
+                .count();
+
+//        Streams are declarative programming allowing us to control collections better.
+    }
+}
